6307c4d7ab96c1897abdb1b5ba485d9bdab6a8b1
Andrea Leardini <andre8244@gmail.com>
2025-08-21 20:42:15 +0200

    add test ingredients

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const mode = import.meta.env.MODE
+
+const API_URL =
+  mode === 'development' ? 'http://localhost:3000/api' : 'https://yumfit-backend.onrender.com/api'
+
+const ingredients = ref([])
+
+const getIngredients = () => {
+  return axios
+    .get(`${API_URL}/ingredients`)
+    .then((res) => (ingredients.value = res.data.ingredients))
+    .catch((error) => {
+      console.error('Error fetching ingredients:', error)
+      ingredients.value = []
+    })
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>YumFit</h1>
+  <p>Mode: {{ mode }}</p>
+  <button @click="getIngredients">Get ingredients</button>
+  <div>
+    {{ ingredients }}
+  </div>
 </template>
 
 <style scoped></style>
